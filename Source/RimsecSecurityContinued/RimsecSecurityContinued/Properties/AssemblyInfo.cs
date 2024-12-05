fc5bc05adb6f77dffca4811044f971a8fcb639a9
Noah <noahshomette@gmail.com>
2024-12-04 21:25:11 -0500

    swap to rimsec Continued

--- Source/RimsecSecurityContinued/RimsecSecurityContinued/Properties/AssemblyInfo.cs
+++ Source/RimsecSecurityContinued/RimsecSecurityContinued/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("51839870-5302-4ddc-91fe-9e17761cc54b")]
+[assembly: Guid("3699c605-2d64-4722-bdda-55c8f11ac7df")]
 
 // Version information for an assembly consists of the following four values:
 //
